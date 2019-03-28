fb96468a5c9e30cb838bc22c0f1ff4207d71d16e
Akihito KIRISAKI <endouryusuke@realglobe.jp>
2019-03-28 19:04:05 +0900

    chore: Initial files

---
+++ src/System/Log/Caster.hs
@@ -0,0 +1,11 @@
+{- |
+Description : Thread-safe, multicast, and not slow logger..
+Module      : System.Log.Caster
+Copyright   : Akihito KIRISAKI (c)
+License     : BSD3
+
+Maintainer  :  Akihito KIRISAKI
+-}
+module System.Log.Caster where
+
+log = undefined
