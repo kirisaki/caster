c54b70cd5765420a8cee64887b9d7497ee55d9d5
Akihito KIRISAKI <endouryusuke@realglobe.jp>
2019-03-29 20:51:48 +0900

    feat: Define LogStr

--- src/System/Log/Caster.hs
+++ src/System/Log/Caster.hs
@@ -1,5 +1,5 @@
 {- |
-Description : Thread-safe, multicast, and not slow logger..
+Description : Thread-safe, multicast, and not slow logger.
 Module      : System.Log.Caster
 Copyright   : Akihito KIRISAKI (c)
 License     : BSD3
@@ -8,4 +8,21 @@ Maintainer  :  Akihito KIRISAKI
 -}
 module System.Log.Caster where
 
+import qualified Data.ByteString.FastBuilder as FB
+import           Data.String                 (IsString (..))
+
 log = undefined
+
+-- | Types which are able to be converted into @Builder@
+class IsString a => LogStr a where
+  toLogStr :: a -> FB.Builder
+
+instance LogStr ST.Text where
+  toLogStr = FB.byteString . STE.encodeUtf8
+
+instance LogStr String where
+  toLogStr = FB.stringUtf8
+
+instance LogStr FB.Builder where
+  toLogStr = id
+
